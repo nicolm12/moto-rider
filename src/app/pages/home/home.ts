9e16658d8d8c61f7d22efa777bc320bc3a64ea7b
NICOL CAMILA MADERO MARTINEZ <nicol.madero@linktic.com>
2025-10-23 18:43:32 -0500

    agregar el footer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // ⬅️ IMPORTAMOS ESTO
 import { AccessoriesComponent } from '../../components/accessories/accessories'; // ⬅️ Asegúrate de que esta ruta sea correcta
 import { HeroSectionComponent } from '../../components/hero-section/hero-section'; // ⬅️ Importado para poder usar <app-hero-section>
-
+import { Footer } from '../../components/footer/footer';
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
@@ -13,7 +13,8 @@ import { HeroSectionComponent } from '../../components/hero-section/hero-section
     CommonModule,
     RouterModule, // ⬅️ LO AÑADIMOS AQUÍ para que routerLink funcione
     AccessoriesComponent, // Componente Accessories
-    HeroSectionComponent  // Componente HeroSection (NUEVO)
+    HeroSectionComponent,
+    Footer // Componente HeroSection (NUEVO)
   ]
 })
 export class HomeComponent {
